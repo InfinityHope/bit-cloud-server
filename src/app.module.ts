383042615bc38426ba1b0930611a3eaa211177de
Nikita <bigbasspoll@gmail.com>
2023-03-15 18:11:27 +0300

    created server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common'
-import { SequelizeModule } from '@nestjs/sequelize'
-import { UserModule } from './user/user.module'
 import { ConfigModule } from '@nestjs/config'
-import { User } from './user/user.model'
-import { RolesModule } from './roles/roles.module'
-import { Role } from './roles/role.model'
-import { UserRoles } from './roles/user-roles.model'
-import { AuthModule } from './auth/auth.module'
-import { TrackModule } from './track/track.module'
-import { Track } from './track/track.model'
-import { FileModule } from './file/file.module'
+import { SequelizeModule } from '@nestjs/sequelize'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import * as path from 'path'
+import { AuthModule } from './auth/auth.module'
+import { FileModule } from './file/file.module'
+import { Role } from './roles/role.model'
+import { RolesModule } from './roles/roles.module'
+import { UserRoles } from './roles/user-roles.model'
+import { Track } from './track/track.model'
+import { TrackModule } from './track/track.module'
+import { User } from './user/user.model'
+import { UserModule } from './user/user.module'
 
 @Module({
     controllers: [],
@@ -26,7 +26,7 @@ import * as path from 'path'
             host: process.env.POSTGRES_HOST,
             port: +process.env.POSTGRES_PORT,
             username: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD,
+            password: String(process.env.POSTGRES_PASSWORD),
             database: process.env.POSTGRES_DB,
             models: [User, Role, UserRoles, Track],
             autoLoadModels: true,
